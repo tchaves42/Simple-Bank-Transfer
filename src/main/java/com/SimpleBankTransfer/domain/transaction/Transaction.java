386e7575c15c8f402632f49baca4fcf546206e5f
thiago <thiagoschaves61@gmail.com>
2023-09-05 14:19:17 -0300

    feat: create Transaction

---
+++ src/main/java/com/SimpleBankTransfer/domain/transaction/Transaction.java
@@ -0,0 +1,37 @@
+package com.SimpleBankTransfer.domain.transaction;
+
+import com.SimpleBankTransfer.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
