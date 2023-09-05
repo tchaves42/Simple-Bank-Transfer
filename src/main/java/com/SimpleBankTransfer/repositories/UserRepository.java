3988ad78deea1bd53613775ced7118ddd2167f62
thiago <thiagoschaves61@gmail.com>
2023-09-05 14:19:34 -0300

    feat: create User repository

---
+++ src/main/java/com/SimpleBankTransfer/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.SimpleBankTransfer.repositories;
+
+import com.SimpleBankTransfer.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+}
