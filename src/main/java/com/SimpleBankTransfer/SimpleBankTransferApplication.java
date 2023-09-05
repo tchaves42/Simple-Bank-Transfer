a23da5ebe4327d9c1688ac2d1e0367c89b3b90c8
thiago <thiagoschaves61@gmail.com>
2023-09-05 14:19:42 -0300

    feat: tests

---
+++ src/main/java/com/SimpleBankTransfer/SimpleBankTransferApplication.java
@@ -0,0 +1,13 @@
+package com.SimpleBankTransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBankTransferApplication.class, args);
+	}
+
+}
