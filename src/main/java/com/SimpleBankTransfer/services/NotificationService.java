1195cd8e5eab4d92ce4c17af8f59d6bec300ca88
Thiago Chaves <127792144+aghiot61@users.noreply.github.com>
2023-09-06 11:07:25 -0300

    Update NotificationService.java (timeout url bug)

--- src/main/java/com/SimpleBankTransfer/services/NotificationService.java
+++ src/main/java/com/SimpleBankTransfer/services/NotificationService.java
@@ -18,11 +18,13 @@ public class NotificationService {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
-        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+        //ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
 
-        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
-            System.out.println("Erro ao enviar notificação.");
-            throw new Exception("O serviço está fora do ar.");
-        }
+       // if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            //System.out.println("Erro ao enviar notificação.");
+            //throw new Exception("O serviço está fora do ar.");
+        //}
+
+        System.out.println("notificacao enviada para o usuario");
     }
 }
